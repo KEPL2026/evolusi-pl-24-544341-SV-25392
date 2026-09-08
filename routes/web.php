b57b851bba143256603ff14f5359dbadb6034005
veen260706 <gurveenderjeetkaur@gmail.com>
2026-09-08 20:26:48 +0700

    feat: add goal controller, routes, and blade view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/goals', [GoalController::class, 'index'])->name('goals.index');
+Route::post('/goals', [GoalController::class, 'store'])->name('goals.store');
+Route::patch('/goals/{goal}/progress', [GoalController::class, 'updateProgress'])->name('goals.progress');
+Route::delete('/goals/{goal}', [GoalController::class, 'destroy'])->name('goals.destroy');
